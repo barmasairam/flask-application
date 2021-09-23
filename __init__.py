c0b294c45889bcbf6b87067a162b6f0bf1b6925c
barmasairam <sairambarma58@gmail.com>
2021-09-23 12:32:14 +0530

    updated the application.

--- __init__.py
+++ __init__.py
@@ -1,66 +1,33 @@
-from flask import Flask, request, session
-from flask_sqlalchemy import SQLAlchemy
-from pytz import UTC
-import pymysql
-pymysql.install_as_MySQLdb()
-from sqlalchemy.sql import func
-
-import re
-from sqlalchemy import func, DateTime, select, types
-from datetime import datetime
-
+from flask import Flask, render_template, request
+from flask_mysqldb import MySQL
 app = Flask(__name__)
 
-app.config['MYSQL_DATABASE_USER'] = 'db_user'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'password'
-app.config['MYSQL_DATABASE_PORT'] = '3306' 
 
-db = SQLAlchemy(app)
+app.config['MYSQL_HOST'] = 'mysql1'
+app.config['MYSQL_USER'] = 'db_user'
+app.config['MYSQL_PASSWORD'] = 'b98eLkdGWz2dFGmmnbbzxq'
+app.config['MYSQL_DB'] = 'MyDB'
 
-class table1(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    received = db.Column(DateTime(timezone=True), default=func.now())
-    data = db.Column(db.String(128))
-
-    def __init__(self, data):
-        self.data = data
-
-def time():
-    time = datetime.now(UTC)
-    time = str(time)
-    time = time[:-13]
-    time_edit1 = time.replace("-", "").replace("-", "").replace(" ", "").replace(":", "").replace(":", "")
-    return time_edit1
+mysql = MySQL(app)
 
 
-@app.route('/gmk', methods=['GET', 'POST'])
-
-def sprinkler():
-    data = request.args.get('d')
-    if data !='':
-        table2 = table1(data)
-        db.session.add(table2)
-        db.session.commit()
-
-
-        last_data_1 = time()
-        return "OUR DATA = *{}<'.{}{}.'>".format(last_data_1, last_data_1, data)
-
-    elif data =='':
-
-        last_rec= db.session.query(db.func.max(table1.id)).scalar()
-        data1 = db.session.query(table1.data).filter(table1.id==last_rec)
-        last_data = data1.first()
-        last_data_1 = str(last_data)
-
-        last_data_1 = last_data_1.replace('(', '').replace("'", "").replace("'", "").replace(",", "").replace(')', '')
-
-        time_edit_2= time()
-
-        return "OUR DATA = *{}<'.{}{}.'>".format(time_edit_2, time_edit_2, last_data_1)
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == "POST":
+        details = request.form
+        firstName = details['fname']
+        lastName = details['lname']
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO MyUsers(firstName, lastName) VALUES (%s, %s)", (firstName, lastName))
+        mysql.connection.commit()
+        cur.close()
+        return 'success'
+    return render_template('index.html')
 
 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=5000,debug=True,use_reloader=True)
     #app.run(debug=True)
+
+
